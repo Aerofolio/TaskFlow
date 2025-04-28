5d6a65c3c4ff1cc18b576fba5976ec9ec021ea92
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2025-04-28 20:59:45 -0300

    Created login activity interactions

--- app/src/main/java/com/example/taskflow/LoginActivity.java
+++ app/src/main/java/com/example/taskflow/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.taskflow;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView textViewRegisterLink;
+    Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,25 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        textViewRegisterLink = findViewById(R.id.textViewRegisterLink);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        textViewRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterUserActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+            }
         });
     }
 }
