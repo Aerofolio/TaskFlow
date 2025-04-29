0c35577beb95f26c7837d7aac27e6f76fd4ac85c
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2025-04-28 21:21:59 -0300

    Created register screen

--- app/src/main/java/com/example/taskflow/RegisterUserActivity.java
+++ app/src/main/java/com/example/taskflow/RegisterUserActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskflow;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterUserActivity extends AppCompatActivity {
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,15 @@ public class RegisterUserActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
