fe0abd049fac55f8feb85f7565833b4386436627
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2025-04-29 21:42:43 -0300

    Create fragment for returnin to previous activity

---
+++ app/src/main/java/com/example/taskflow/HeaderFragment.java
@@ -0,0 +1,49 @@
+package com.example.taskflow;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HeaderFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HeaderFragment extends Fragment {
+    ImageButton imageButtonReturn;
+
+    public HeaderFragment() {
+    }
+
+    public static HeaderFragment newInstance() {
+        HeaderFragment fragment = new HeaderFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_header, container, false);
+        imageButtonReturn = view.findViewById(R.id.imageButtonReturn);
+        imageButtonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+        return view;
+    }
+}
